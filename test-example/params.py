69101a9e70ce6b8f9129096f00d68c89be58b290
jeisonchuquin <jeison.chuquin@hotmail.com>
2026-02-16 12:27:10 -0500

    wandb test

---
+++ test-example/params.py
@@ -0,0 +1,5 @@
+WANDB_PROJECT = "cicd-quickstart-jc"
+ENTITY = 'jeison-chuquin-ds'
+BDD_CLASSES = {i:c for i,c in enumerate(['background', 'road', 'traffic light', 'traffic sign', 'person', 'vehicle', 'bicycle'])}
+RAW_DATA_AT = 'jeison-chuquin-ds/cicd-quickstart-jc/bdd_simple_1k'
+PROCESSED_DATA_AT = 'jeison-chuquin-ds/cicd-quickstart-jc/bdd_simple_1k_split'
